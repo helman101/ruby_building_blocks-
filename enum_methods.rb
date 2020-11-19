9af5da4da975bb8c1d4464692a8d35f0b74b5efb
helman101 <helmanandresortegon@gmail.com>
2020-11-19 13:49:33 +0100

    Code my_select method

--- enum_methods.rb
+++ enum_methods.rb
@@ -20,6 +20,17 @@ module Enumerable
       puts 'No Block Given'
     end
   end
+
+  def my_select
+    if instance_of?(Hash)
+      result = {}
+      my_each { |key, value| result[key] = value if yield(key, value) == true }
+    else
+      result = []
+      my_each { |value| result.push(value) if yield(value) == true }
+    end
+    result
+  end
 end
 
 hash = { 'twenty' => 20, 'thirty' => 30, 'fourty' => 40 }
@@ -28,3 +39,6 @@ hash = { 'twenty' => 20, 'thirty' => 30, 'fourty' => 40 }
 hash.my_each { |key, value| puts "this is my key #{key} and value #{value}" }
 # Each with index method
 hash.my_each_with_index { |value, index| puts "this is my key #{value[0]} and value #{value[1]} and index #{index}" }
+# Select method
+puts(hash.my_select { |key, value| value > 20 && key.instance_of?(String) })
+puts([20, 30, 40].my_select { |value| value > 20 })
